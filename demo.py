702aa6195a5bbe2794b3a4cbc62ba069ac0da430
zainab <zainab.zulfiqar@gmail.com>
2025-01-27 13:50:14 +0500

    Modified demofile

--- demo.py
+++ demo.py
@@ -1,9 +1,13 @@
+"""
+This module demonstrates a simple function example.
+"""
+
 def demo():
     """
-    demo
+    A demo function that returns the value 5.
     """
     a = 5
     return a
 
-
 demo()
+
